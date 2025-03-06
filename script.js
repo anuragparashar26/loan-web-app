a0d7ad808314b90edd6297690f6c4296102d4895
Anurag Parashar <111885714+anuragparashar26@users.noreply.github.com>
2025-03-06 19:02:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('navLinks');
+    const hamburger = document.getElementById('hamburger');
+    navLinks.classList.toggle('active'); 
+    hamburger.classList.toggle('active');  
+
+    if (navLinks.classList.contains('active')) {
+        const links = navLinks.querySelectorAll('a');
+        links.forEach(link => {
+            link.addEventListener('click', () => {
+                navLinks.classList.remove('active');
+                hamburger.classList.remove('active');  
+            });
+        });
+        window.addEventListener('scroll', closeMenuOnScroll);
+    } else {
+        window.removeEventListener('scroll', closeMenuOnScroll);
+    }
+}
+
+function toggleAnswer(element) {
+    let answer = element.nextElementSibling;
+    answer.style.display = (answer.style.display === "block") ? "none" : "block";
+}
